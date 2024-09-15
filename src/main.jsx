fd1ea1612ac40cb7d762486c5776af335aaa9465
Sunwoo Jun <37898596+whitersun@users.noreply.github.com>
2024-09-16 00:56:25 +0800

    add tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
+import "./assets/scss/tailwind.scss"
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app'
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
