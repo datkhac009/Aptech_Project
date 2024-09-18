2b529f665febbd91f21ed20a4c6e04c263643af9
Đạt Nguyễn khắc <101082932+datkhac009@users.noreply.github.com>
2024-09-18 15:36:30 +0700

    Done

--- src/components/headers/index.jsx
+++ src/components/headers/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import "./index.scss"
-
+    
 import { Link } from 'react-router-dom'
 
 
@@ -11,26 +11,27 @@ const Header = () => {
         const handleScroll = () => {
             setIsheight(window.scrollY > 100)
         };
-      
+        
         window.addEventListener('scroll', handleScroll);
       
         return () => {
           window.removeEventListener('scroll', handleScroll);
         };
-      }, []);
+      }, [isheight]);
     console.log(isheight)
     // console.log(scrollY)
     return (
         <header className="header">
-            <nav className={`header-block transition-all ${isheight ?'hidden' : 'block'}`}>
+            <nav className={`header-block transition-all ${isheight ?'header-small' : ''}`}>
                 <div className="logo">
-                    <img src="" alt=""></img>
+                    <img src="/src/assets/img/logo.png" alt=""></img>
                 </div>
-                <div className="search">
+                <div className="search  ">
                 <form>
                     <fieldset>
                         <input type="text" placeholder="Search by product or keyword..."/>
-                        <button type="submit">search</button>
+                        <button type="submit">  search</button>
+        
                     </fieldset>
                 </form>
                 </div>
@@ -41,7 +42,7 @@ const Header = () => {
                     <div className="account">
                         <i></i>
                         <a href="#">Sign in </a>
-                        <span>/</span>
+                        <span>/ </span>
                         <a href="#">Register</a>
                     </div>
                     <div className="shopping">
@@ -49,7 +50,7 @@ const Header = () => {
                     </div>
                 </div>
             </nav>
-            <nav className="header__nav">
+            {/* <nav className="header__nav">
                 <ul className="header__list">
                     <li className="header__item">
                         <Link className="header__link" to="/homepage">Home</Link>
@@ -58,7 +59,7 @@ const Header = () => {
                         <Link className="header__link" to="/about">About</Link>
                     </li>
                 </ul>
-            </nav>
+            </nav> */}
         </header>
     )
 }
