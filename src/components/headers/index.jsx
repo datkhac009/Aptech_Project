de7815a6cc42323356243416ee236d989ce70ed9
Đạt Nguyễn khắc <101082932+datkhac009@users.noreply.github.com>
2024-09-19 22:11:50 +0700

    homepage

--- src/components/headers/index.jsx
+++ src/components/headers/index.jsx
@@ -1,41 +1,52 @@
 import { useEffect, useState } from "react"
 import "./index.scss"
-    
+
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCartShopping, faMagnifyingGlass, faPhone, faUser } from "@fortawesome/free-solid-svg-icons"
 
 
 const Header = () => {
-    const[isheight, setIsheight] = useState(false)
-
+    const [isheight, setIsheight] = useState(false)
+    const [ismenuchild, setIsmenuchild] = useState(false)
+    const [isIndex,setIndex]   = useState(0)
+    const handleClickMenuchild = (index) => {
+        setIsmenuchild(!ismenuchild)
+        setIndex(index)
+        
+       
+      
+    }
     useEffect(() => {
         const handleScroll = () => {
             setIsheight(window.scrollY > 100)
         };
-        
+
         window.addEventListener('scroll', handleScroll);
-      
+
         return () => {
-          window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener('scroll', handleScroll);
         };
-      }, [isheight]);
+    }, [isheight]);
     console.log(isheight)
     // console.log(scrollY)
+    console.log(ismenuchild)
+    console.log(isIndex)
     return (
         <header className="header">
-            <nav className={`header-block transition-all ${isheight ?'header-small' : ''}`}>
+            {/* headerblock */}
+            <nav className={`header-block transition-all ${isheight ? 'header-small' : ''} `}>
                 <div className="logo">
                     <img src="/src/assets/img/logo.png" alt=""></img>
                 </div>
                 <div className="search  ">
-                <form>
-                    <fieldset>
-                        <input type="text" placeholder="Search by product or keyword..."/>
-                        <button type="submit" className="search-btn w-[50px] px-4 py-0 mb-0 bg-[#ddd] border-none text-[#333]"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
-        
-                    </fieldset>
-                </form>
+                    <form>
+                        <fieldset>
+                            <input type="text" placeholder="Search by product or keyword..." />
+                            <button type="submit" className="search-btn w-[50px] px-4 py-0 mb-0 bg-[#ddd] border-none text-[#333]"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+
+                        </fieldset>
+                    </form>
                 </div>
                 <div className="user">
                     <div className="phone">
@@ -52,6 +63,38 @@ const Header = () => {
                     </div>
                 </div>
             </nav>
+
+            {/* menu */}
+            <div className="navMenuPages">
+                <ul className="navMenuPages__list">
+                    <li onClick={() => { handleClickMenuchild(1) }} className={`navMenuPages__parent ${ismenuchild && isIndex === 1  ? 'active' : 'hiddens'}`}> <a href="#">Spices</a>
+                        <ul className="navMenuPages__child ">
+                            <li> <a href="#">Spices:A_C</a> </li>
+                            <li> <a href="#">Spices:A_C</a> </li>
+                            <li> <a href="#">Spices:A_C</a> </li>
+                            <li> <a href="#">Spices:A_C</a> </li>
+                            <li> <a href="#">Spices:A_C</a> </li>
+                            <li> <a href="#">Spices:A_C</a> </li>
+                            <li> <a href="#">Spices:A_C</a> </li>
+                            <li> <a href="#">Spices:A_C</a> </li>
+                        </ul>
+                    </li>
+                    <li onClick={() => { handleClickMenuchild(2) }} className={`navMenuPages__parent ${ismenuchild && isIndex === 2  ? 'active' : 'hiddens'}`}> <a href="#">Spices</a>
+                        <ul className="navMenuPages__child ">
+                            <li> <a href="#">Spices:A_C</a> </li>
+                            <li> <a href="#">Spices:A_C</a> </li>
+                            <li> <a href="#">Spices:A_C</a> </li>
+                            <li> <a href="#">Spices:A_C</a> </li>
+                            <li> <a href="#">Spices:A_C</a> </li>
+                            <li> <a href="#">Spices:A_C</a> </li>
+                            <li> <a href="#">Spices:A_C</a> </li>
+                            <li> <a href="#">Spices:A_C</a> </li>
+                        </ul>
+                    </li>
+                    <li> <a href="#">Dehydrated Fruits &amp; Vegetables</a></li>
+                </ul>
+
+            </div>
             {/* <nav className="header__nav">
                 <ul className="header__list">
                     <li className="header__item">
