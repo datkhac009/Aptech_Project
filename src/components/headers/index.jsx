59faa1f143f27869da1054470edbf684fd670e53
Đạt Nguyễn khắc <101082932+datkhac009@users.noreply.github.com>
2024-09-21 14:35:19 +0700

    Update

--- src/components/headers/index.jsx
+++ src/components/headers/index.jsx
@@ -3,7 +3,7 @@ import "./index.scss"
 
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCartShopping, faMagnifyingGlass, faPhone, faUser } from "@fortawesome/free-solid-svg-icons"
+import { faAngleDown, faCartShopping, faMagnifyingGlass, faPhone, faUser } from "@fortawesome/free-solid-svg-icons"
 
 
 const Header = () => {
@@ -11,11 +11,13 @@ const Header = () => {
     const [ismenuchild, setIsmenuchild] = useState(false)
     const [isIndex,setIndex]   = useState(0)
     const handleClickMenuchild = (index) => {
-        setIsmenuchild(!ismenuchild)
         setIndex(index)
-        
-       
-      
+        if(isIndex === index){
+            setIsmenuchild(!ismenuchild)
+        }
+        else{
+            setIsmenuchild(true)
+        } 
     }
     useEffect(() => {
         const handleScroll = () => {
@@ -28,6 +30,7 @@ const Header = () => {
             window.removeEventListener('scroll', handleScroll);
         };
     }, [isheight]);
+
     console.log(isheight)
     // console.log(scrollY)
     console.log(ismenuchild)
@@ -35,7 +38,7 @@ const Header = () => {
     return (
         <header className="header">
             {/* headerblock */}
-            <nav className={`header-block transition-all ${isheight ? 'header-small' : ''} `}>
+            <nav className={`header-block ${isheight ? 'header-small' : ''} `}>
                 <div className="logo">
                     <img src="/src/assets/img/logo.png" alt=""></img>
                 </div>
@@ -65,35 +68,46 @@ const Header = () => {
             </nav>
 
             {/* menu */}
-            <div className="navMenuPages">
-                <ul className="navMenuPages__list">
-                    <li onClick={() => { handleClickMenuchild(1) }} className={`navMenuPages__parent ${ismenuchild && isIndex === 1  ? 'active' : 'hiddens'}`}> <a href="#">Spices</a>
+            <div className={`navMenuPages ${isheight ? 'hiddens' : ''}`}>
+                <ul className="navMenuPages__list ">
+                    <li onClick={() => { handleClickMenuchild(1) }} className={`navMenuPages__parent ${ismenuchild && isIndex === 1  ? 'active' : 'hiddens'}`}> <a href="#">Spices <FontAwesomeIcon icon={faAngleDown} /></a>
                         <ul className="navMenuPages__child ">
-                            <li> <a href="#">Spices:A_C</a> </li>
-                            <li> <a href="#">Spices:A_C</a> </li>
-                            <li> <a href="#">Spices:A_C</a> </li>
-                            <li> <a href="#">Spices:A_C</a> </li>
-                            <li> <a href="#">Spices:A_C</a> </li>
-                            <li> <a href="#">Spices:A_C</a> </li>
-                            <li> <a href="#">Spices:A_C</a> </li>
-                            <li> <a href="#">Spices:A_C</a> </li>
+                            <li> <a href="#">Spices: A - C</a> </li>
+                            <li> <a href="#">Spices: D - F</a> </li>
+                            <li> <a href="#">Spices: G - I</a> </li>
+                            <li> <a href="#">Spices: J - L</a> </li>
+                            <li> <a href="#">Spices: M - O</a> </li>
+                            <li> <a href="#">Spices: P - R</a> </li>
+                            <li> <a href="#">Spices: S - U</a> </li>
+                            <li> <a href="#">Spices: V - Z</a> </li>
                         </ul>
                     </li>
-                    <li onClick={() => { handleClickMenuchild(2) }} className={`navMenuPages__parent ${ismenuchild && isIndex === 2  ? 'active' : 'hiddens'}`}> <a href="#">Spices</a>
+                    <li onClick={() => { handleClickMenuchild(2) }} className={`navMenuPages__parent ${ismenuchild && isIndex === 2  ? 'active' : 'hiddens'}`}> <a href="#">Seasonings <FontAwesomeIcon icon={faAngleDown} /></a>
                         <ul className="navMenuPages__child ">
-                            <li> <a href="#">Spices:A_C</a> </li>
-                            <li> <a href="#">Spices:A_C</a> </li>
-                            <li> <a href="#">Spices:A_C</a> </li>
-                            <li> <a href="#">Spices:A_C</a> </li>
-                            <li> <a href="#">Spices:A_C</a> </li>
-                            <li> <a href="#">Spices:A_C</a> </li>
-                            <li> <a href="#">Spices:A_C</a> </li>
-                            <li> <a href="#">Spices:A_C</a> </li>
+                            <li> <a href="#">Seasonings: A - C</a> </li>
+                            <li> <a href="#">Seasonings: D - F</a> </li>
+                            <li> <a href="#">Seasonings: G - I</a> </li>
+                            <li> <a href="#">Seasonings: J - L</a> </li>
+                            <li> <a href="#">Seasonings: M - O</a> </li>
+                            <li> <a href="#">Seasonings: P - R</a> </li>
+                            <li> <a href="#">Seasonings: S - U</a> </li>
+                            <li> <a href="#">Seasonings: V - Z</a> </li>
                         </ul>
                     </li>
-                    <li> <a href="#">Dehydrated Fruits &amp; Vegetables</a></li>
+                    <li> <a href="#" className="navMenuPages__parent "> Gourmet Salts</a></li>
+                    <li> <a href="#" className="navMenuPages__parent"> Organic</a></li>
+                    <li> <a href="#" className="navMenuPages__parent"> Gifts &amp; Sets</a></li>
+                    <li onClick={() => { handleClickMenuchild(3) }} className={`navMenuPages__parent ${ismenuchild && isIndex === 3  ? 'active' : 'hiddens'}`}> <a href="#">Member Exclusives <FontAwesomeIcon icon={faAngleDown} /></a>
+                        <ul className="navMenuPages__child ">
+                            <li> <a href="#">Member Products</a> </li>
+                            <li> <a href="#">Become a Member</a> </li>
+                        </ul>
+                    </li>    
                 </ul>
 
+            </div>
+            <div className="header__bottom">
+                <p>FREE Shipping on $25 or more! Just $5 for all other orders.</p>
             </div>
             {/* <nav className="header__nav">
                 <ul className="header__list">
